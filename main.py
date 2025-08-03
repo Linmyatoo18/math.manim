790350f605ae686eaf13c29f3a0baa0693e908bb
Linmyatoo_18 <linmyatoo1010@gmail.com>
2025-08-03 15:48:51 +0630

    .gitignore

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ import numpy as np
 class waves(Scene):
     def construct(self):
         a = Axes(x_range=[-10,10], y_range=[-2,2])
-        
+        graph = a.plot(lambda x: np.sin(x), color=RED)
         self.add(a)
